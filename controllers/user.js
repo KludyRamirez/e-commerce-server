aa3fe8140027cab677b7a4b59c07719e4a3b0d9f
Kludy Ramirez <ramirezkludy23@gmail.com>
2023-05-14 06:45:09 +0800

    update

--- controllers/user.js
+++ controllers/user.js
@@ -155,9 +155,9 @@ exports.createOrder = async (req, res) => {
 exports.orders = async (req, res) => {
   let user = await User.findOne({ email: req.user.email }).exec();
 
-  let userOrders = await Order.find({ orderedBy: user._id })
+  let userOrders = await Order.find({ orderdBy: user._id })
     .populate("products.product")
-    .populate("orderedBy", "name email address")
+    .populate("orderdBy", "name email address")
     .exec();
 
   res.json(userOrders);
